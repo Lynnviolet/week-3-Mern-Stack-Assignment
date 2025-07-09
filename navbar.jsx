9f8d7e938ad98c3d96c4494905bc693a9dff9aaa
LYNN VIOLET WANJIRU <shikkslynn@gmail.com>
2025-07-09 12:51:18 +0300

    Update navbar.jsx

--- navbar.jsx
+++ navbar.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ThemeToggle from './ThemeToggle';
 
 const Navbar = () => {
   return (
     <nav className="bg-blue-600 text-white px-6 py-4 flex justify-between items-center">
       <h1 className="text-xl font-bold">React Task App</h1>
-      <div className="space-x-4">
+      <div className="flex gap-4 items-center">
         <Link to="/" className="hover:underline">Home</Link>
         <Link to="/tasks" className="hover:underline">Tasks</Link>
         <Link to="/api" className="hover:underline">API</Link>
+        <ThemeToggle />
       </div>
     </nav>
   );
